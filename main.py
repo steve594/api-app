7286c51c0b5013f4982ea8f9180776a0a5da2bab
Steve Njock <stevenjockchristian@gmail.com>
2023-12-19 22:12:09 -0500

    Access the news titles and description.

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import requests
+from email_sender import email_sender
 #Key
 api_keys = "251e29fb7dc241848c84ea29dcb7b47f"
 
 #Endpoints
-api_url = "https://newsapi.org/v2/everything?q=tesla&from=2023-11-19&sortBy=publishedAt&apiKey=251e29fb7dc241848c84ea29dcb7b47f"
+api_url = "https://newsapi.org/v2/everything?q=tesla&from=2023-11-20&sortBy=publishedAt&apiKey=251e29fb7dc241848c84ea29dcb7b47f"
 
+#Make request
 request = requests.get(api_url)
 
 #Get a dictionary with data
 display_content = request.json()
 
-#Access the article titles, description and author
-for articles in display_content["articles"]:
-    print(articles["description"])
-    print(articles["title"])
-    print(articles["author"])
+#Access the article titles and description
+body = ""
+for article in display_content["articles"]:
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+email_sender(message=body)
+
